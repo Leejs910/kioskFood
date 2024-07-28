dfbc9943df157bff859e4118d8bfcc85e5dde618
Leejs910 <103399477+Leejs910@users.noreply.github.com>
2024-07-28 22:52:09 +0900

    Add files via upload

---
+++ foodData.js
@@ -0,0 +1,177 @@
+const foodData =[
+    { 
+        title: "용우동",
+        imgNum: "A-001",
+        foodName:"용우동",
+        price: 5500,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-002",
+        foodName:"제육볶음우동+밥",
+        price: 8500,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-003",
+        foodName:"새우튀김우동",
+        price: 7000,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-004",
+        foodName:"스페셜 어묵우동",
+        price: 7000,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-005",
+        foodName:"해물짬뽕우동+밥",
+        price: 8500,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-006",
+        foodName:"우동알밥세트",
+        price: 9000,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-007",
+        foodName:"우동돈가스세트",
+        price: 10000,
+    },
+    {   
+        title: "용우동",
+        imgNum: "A-008",
+        foodName:"순두부짬뽕우동+밥",
+        price: 8500,
+    },
+    {   
+        title: "면의 맛",
+        imgNum: "B-001",
+        foodName:"용우동",
+        price: 5500,
+    },
+    {   
+        title: "면의 맛",
+        imgNum: "B-003",
+        foodName:"쫄면돈까스세트",
+        price: 8500,
+    },
+    
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-001",
+        foodName:"등심돈까스",
+        price: 8000,
+    },
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-002",
+        foodName:"고구마치즈돈까스",
+        price: 9000,
+    },
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-003",
+        foodName:"치즈돈까스",
+        price: 9000,
+    },
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-004",
+        foodName:"왕돈까스",
+        price: 10000,
+    },
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-005",
+        foodName:"매운돈까스",
+        price: 9000,
+    },
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-006",
+        foodName:"함박스테이크+안심치킨",
+        price: 8500,
+    },
+    {   
+        title: "돈까스의 맛",
+        imgNum: "C-007",
+        foodName:"돈부리",
+        price: 8000,
+    },
+    
+    {   
+        title: "밥의 맛",
+        imgNum: "D-001",
+        foodName:"순두부찌개",
+        price: 7500,
+    },
+    {   
+        title: "밥의 맛",
+        imgNum: "D-002",
+        foodName:"양푼이 비빔밥",
+        price: 7500,
+    },
+    {   
+        title: "밥의 맛",
+        imgNum: "D-003",
+        foodName:"돌솥육개장",
+        price: 8000,
+    },
+    {   
+        title: "밥의 맛",
+        imgNum: "C-004",
+        foodName:"돌솥김치알밥",
+        price: 8000,
+        
+    },
+    
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-001",
+        foodName:"돌솥콘치즈닭갈비",
+        price: 8500,
+    },
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-002",
+        foodName:"매운치즈삼겹살덮밥",
+        price: 8500,
+    },
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-003",
+        foodName:"치킨마요",
+        price: 7500,
+    },
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-004",
+        foodName:"데리야끼치킨덮밥",
+        price: 7500,
+    },
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-005",
+        foodName:"제육덮밥",
+        price: 8500,
+    },
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-006",
+        foodName:"해물삼겹불고기덮밥",
+        price: 8500,
+    },
+    {   
+        title: "덮밥의 맛",
+        imgNum: "E-007",
+        foodName:"용버섯덮밥",
+        price: 8500,
+    },
+]
+
+
+
